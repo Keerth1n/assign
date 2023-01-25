7c7130cfb4e3c64a0a6b0466d6174d404c9d2080
Keerthan <120743232+Keerth1n@users.noreply.github.com>
2023-01-25 20:47:50 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int M;
+    cout << "Enter a number:";
+    cin >> M;
+    if (M % 3 == 0 && M % 5 == 0)
+        cout << "Good Number";
+    else if (M % 3 == 0 && M % 5 != 0)
+        cout << "Bad Number";
+    else if (M % 3 != 0 && M % 5 == 0)
+        cout << "Poor Number";
+    else
+        cout << "-1";
+    return 0;
+}
